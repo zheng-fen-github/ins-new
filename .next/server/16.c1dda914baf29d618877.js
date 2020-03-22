324a52d494144ba78cf8afa53809737499eaa40a
zhengfen <1582893052@qq.com>
2020-03-22 17:51:20 +0800

    testok

---
+++ .next/server/16.c1dda914baf29d618877.js
@@ -0,0 +1,34 @@
+exports.ids = [16];
+exports.modules = {
+
+/***/ "d1hE":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+function Dymain() {
+  return __jsx("div", null, "hello word", __jsx("img", {
+    src: "cat.jps",
+    alt: ""
+  }), __jsx("img", {
+    src: "cat.jps",
+    alt: ""
+  }), __jsx("img", {
+    src: "cat.jps",
+    alt: ""
+  }), __jsx("img", {
+    src: "cat.jps",
+    alt: ""
+  }));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Dymain);
+
+/***/ })
+
+};;
